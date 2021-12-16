e7a5550cf7e859b2669d5cba2186ba70f5ddeaa2
HCLTECH\shivam.gulati <Shivam.Gulati7@t-mobile.com>
2021-12-16 12:12:00 +0530

    All commited

--- target/test-classes/features/login.feature
+++ target/test-classes/features/login.feature
@@ -1,10 +1,7 @@
-
 Feature: Log in Funtionality
 
   Scenario: Sign in  Functionality
-    Given user open Amazon Home page
-    When user mouseHover and click on sign in button
-#    And user input email "mirazakon@outlook.com"
-#    And user click on continue
-#    And user enter passwords "TalentTech77"
-#    And user click on Sign in
+    Given user should open T-Mobile login page
+    When User enter user name and password
+   And user click on login button
+#    Then user verify the home page
--- test-output/report.js
+++ test-output/report.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/login.feature");
 formatter.feature({
-  "line": 2,
+  "line": 1,
   "name": "Log in Funtionality",
   "description": "",
   "id": "log-in-funtionality",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 4,
+  "line": 3,
   "name": "Sign in  Functionality",
   "description": "",
   "id": "log-in-funtionality;sign-in--functionality",
@@ -15,27 +15,20 @@ formatter.scenario({
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 5,
-  "name": "user open Amazon Home page",
+  "line": 4,
+  "name": "user should open T-Mobile login page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 6,
-  "name": "user mouseHover and click on sign in button",
+  "line": 5,
+  "name": "User enter user name and password",
   "keyword": "When "
 });
-formatter.match({
-  "location": "stepDef.user_open_amazon_home_page()"
-});
-formatter.result({
-  "duration": 43828236100,
-  "status": "passed"
+formatter.step({
+  "line": 6,
+  "name": "user click on login button",
+  "keyword": "And "
 });
 formatter.match({
-  "location": "stepDef.user_mousehover_and_click_on_sign_in_button()"
-});
-formatter.result({
-  "duration": 7019973300,
-  "status": "passed"
-});
+  "location": "stepDef.user_should_open_tmobile_login_page()"
 });
